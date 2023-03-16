3c0eb6c6d374e750ebdc7799b3775afa71f534cc
Kyle Thornton <kthornton@mavens.com>
2023-03-16 14:40:52 -0400

    setting up the react app

---
+++ frontend/src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Kwips</h1>
+        <p className="text-muted">A komodo webhook proxy service</p>
+      </div>
+    </div>
+  );
+}
